ed69dca8d9a0c3edffc0d967c672c7f39b501e5d
Rainis <118337100+Rasmuselemeeldivadmehed12@users.noreply.github.com>
2023-03-22 10:19:45 +0200

    Add files via upload

---
+++ Rainise LINQ/SumJaSkipwhile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Andmed, mille peal LINQ päringuid teha
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // SkipWhile - jätab vahele elemendid, mis rahuldavad tingimust
+        var skipwhile = numbers.SkipWhile(x => x < 5);
+
+        Console.WriteLine("SkipWhile:");
+
+        // Prindib välja elemendid, mis jäid alles
+        foreach (var number in skipwhile)
+        {
+            Console.WriteLine(number);
+        }
+
+        // Sum - leiab arvude summa
+        int summa = numbers.Sum();
+
+        Console.WriteLine("\nSum:");
+
+        // Prindib välja arvude summa
+        Console.WriteLine(summa);
+    }
+}
